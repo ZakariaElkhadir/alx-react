260f18e03101bdffa2553e15339745e8cba69938
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-05 11:36:06 +0100

    chore: Refactor unit tests for utils module

--- 0x01-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -1,45 +1,25 @@
 // utils.test.js
-const { getFullYear, getFooterCopy, getLatestNotification } = require('./utils');
-
-
-describe(getFullYear, () => {
-  it('should return the correct year', () => {
-    // Mock the current date
-    const mockDate = new Date(2023, 0, 1); // January 1, 2023
-    jest.spyOn(global, 'Date').mockImplementation(() => mockDate);
-
-    // Call the function and check the result
-    const year = getFullYear();
-    expect(year).toBe(2023);
-
-    // Restore the original Date object
-    global.Date.mockRestore();
-  });
+const { getFullYear, getFooterCopy, getLatestNotification } = require('./utils')
+describe('getFullYear', () => {
+    it('should return the correct year', () => {
+        
+        expect(getFullYear()).toEqual(new Date().getFullYear());
+    })
 });
 
-describe(getFooterCopy, () => {
-    it('should return the correct string when isIndex is true', () => {
-        const isIndex = true;
-        const footerCopy = getFooterCopy(isIndex);
-        expect(footerCopy).toBe('Copywright © 2023 - All rights reserved');
+describe('getFooterCopy', () => {
+    it('should return "Holberton school" when the argument is true',() => {
+        const result = getFooterCopy(true);
+        expect(result).toBe('Holberton school');
     });
-
-    it('should return the correct string when isIndex is false', () => {
-        const isIndex = false;
-        const footerCopy = getFooterCopy(isIndex);
-        expect(footerCopy).toBe('Copywright © 2023 - All rights reserved');
-    });
+    it('should return Holberton School main dashboard', () => {
+        const result = getFooterCopy(false);
+        expect(result).toBe('Holberton School main dashboard')
+    })
 });
 
-describe(getLatestNotification, () => {
-    it('should return a string', () => {
-        const notification = getLatestNotification();
-        expect(typeof notification).toBe('string');
-    });
-
-    it('should return the correct notification', () => {
-        const expectedNotification = '<strong>Urgent requirement</strong> - complete by EOD';
-        const notification = getLatestNotification();
-        expect(notification).toBe(expectedNotification);
-    });
+describe('getLatestNotification', () => {
+    it('should return Urgent requirement - complete by EOD', () => {
+        expect('<strong>Urgent requirement</strong> - complete by EOD').toBe(getLatestNotification())
+    })
 });
