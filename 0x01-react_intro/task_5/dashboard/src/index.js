e3f1f788ca30766b6f80102c8b10e22bff341f13
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-06 12:51:05 +0100

    Refactor webpack configuration and add devDependencies for webpack-cli and webpack-dev-server

---
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,5 @@
+import tripleMe from './tripleme'
+const validator  = require('validator')
+console.log(validator.isEmail('john@test.com'))
+
+console.log(tripleMe(20))
---
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        rules: [
+            {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i, // Handle image files
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                        },
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                            },
+                            optipng: {
+                                enabled: true,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+        },
+        
+        
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        hot: true,
+        open: true
+    },
+    mode: 'development'
+}
