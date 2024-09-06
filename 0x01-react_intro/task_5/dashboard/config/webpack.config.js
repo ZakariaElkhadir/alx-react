74521bff94f21a1ef5a9bcb8549442d6bb6a9860
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-06 10:29:51 +0100

    Refactor project structure and remove unused files

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,55 +1,36 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
+    mode: 'development', // or 'production' depending on your needs
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js', // Name of the output file
+        path: path.resolve(__dirname, '../dist'), // Output directory
     },
-    devtool: 'inline-source-map',
+    devtool: 'inline-source-map', // For source maps
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
             },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 use: [
                     {
-                        loader: 'file-loader',
+                        loader: 'url-loader',
                         options: {
-                            name: '[path][name].[ext]'
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
+                            limit: 8192, // Inline images smaller than 8kb
+                        },
+                    },
+                ],
+            },
+        ],
     },
     devServer: {
-        contentBase: path.resolve(__dirname, 'dist'),
+        contentBase: path.join(__dirname, '../dist'),
+        compress: true,
+        port: 8080, // Port for the dev server
         hot: true,
-        open: true
     },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
 };
