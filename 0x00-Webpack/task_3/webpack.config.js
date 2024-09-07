eeb60c9cf6a7a70a07eae050cd70983ad150ed27
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-07 11:46:58 +0100

    Refactor webpack configuration to optimize file bundling and add Jest testing

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,66 +1,42 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-	plugins: [
-		new HTMLWebpackPlugin({
+  entry: {
+    header: {
+      import: './modules/header/header.js',
+      dependOn: 'shared',
+    },
+    footer: {
+      import: './modules/footer/footer.js',
+      dependOn: 'shared',
+    },
+    shared: 'jquery',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    static: path.join(__dirname, './public'),
+    open: true,
+    port: 8564,
+  },
+  performance: {
+    maxAssetSize: 1000000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      temmplate:"./public/index.html",
 			filename: './index.html',
 		}),
 		new CleanWebpackPlugin(),
-	],
-	devtool: 'inline-source-map',
-	mode: 'development',
-	entry: {
-		header: {
-			import: './modules/header/header.js',
-			dependOn: 'shared',
-		},
-		body: {
-			import: './modules/body/body.js',
-			dependOn: 'shared',
-		},
-		footer: {
-			import: './modules/footer/footer.js',
-			dependOn: 'shared',
-		},
-		shared: 'jquery',
-	},
-	output: {
-		path: path.resolve(__dirname, 'public'),
-		filename: '[name].bundle.js',
-	},
-	optimization: {
-		splitChunks: {
-			chunks: 'all',
-		},
-	},
-	devServer: {
-		static: path.join(__dirname, './public'),
-		open: true,
-		port: 8564,
-	},
-	performance: {
-		maxAssetSize: 1000000,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-				use: [
-					{
-						loader: ['file-loader', 'image-webpack-loader'],
-						options: {
-							bypassOnDebug: true,
-							disable: true,
-						},
-					},
-				],
-			},
-		],
-	},
+  ],
 };
