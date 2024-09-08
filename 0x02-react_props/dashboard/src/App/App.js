0e1a42812841658aa22dced4d79bda28bd3ef5a6
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-08 10:21:19 +0100

    Refactor file paths and imports in body.js, body.css, header.js, header.css, footer.css, main.css, footer.js, and index.html

---
+++ 0x02-react_props/dashboard/src/App/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <hr />
+      <Login/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
