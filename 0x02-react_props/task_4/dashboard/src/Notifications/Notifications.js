7270de584185139816a51b0b0191f9ae9d24b71b
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-10 10:28:43 +0100

    Refactor file paths and imports in Notifications.js

--- 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_4/dashboard/src/Notifications/Notifications.js
@@ -4,50 +4,46 @@ import PropTypes from "prop-types";
 import exit from "../assets/x.png";
 import { getLatestNotification } from "../utils/utils";
 import NotificationItem from "./NotificationItem";
-
-const Notifications = ({ displayDrawer }) => {
+import propTypes from "prop-types";
+const Notifications = ({displayDrawer}) => {
   return (
     <>
-      <div className="menuItem">
-        Your notification
-      </div>
-      {displayDrawer && (
-        <div className="Notifications">
-          <button
-            style={{
-              float: "right",
-              border: "none",
-              background: "none",
-              padding: 0,
-            }}
-            aria-label="Close"
-            onClick={() => console.log("Close button has been clicked")}
-          >
-            <img src={exit} alt="Close" width={10} />
-          </button>
-          <p>Here is the list of notifications</p>
-          <ul>
-            <NotificationItem type="default" value="New course available" />
-            <NotificationItem type="urgent" value="New resume available" />
-            <NotificationItem
-              type="urgent"
-              html={{
-                __html: "<strong>Urgent requirement</strong> - complete by EOD",
-              }}
-            />
-          </ul>
-        </div>
-      )}
+    <div className="menuItem">
+          Your notification
+    </div>
+    {displayDrawer &&(<div className="Notifications">
+      <button
+        style={{
+          float: "right",
+          border: "none",
+          background: "none",
+          padding: 0,
+        }}
+        aria-label="Close"
+        onClick={() => console.log("Close button has been clicked")}
+      >
+        <img src={exit} alt="Close" width={10} />
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <NotificationItem type="default" value="New course available" />
+        <NotificationItem type="urgent" value="New resume available" />
+        <NotificationItem
+          type="urgent"
+          html={{
+            __html: "<strong>Urgent requirement</strong> - complete by EOD",
+          }}
+        />
+      </ul>
+    </div>
+    )}
     </>
   );
 };
-
 Notifications.propTypes = {
-  displayDrawer: PropTypes.bool,
+  displayDrawer: propTypes.bool,
 };
-
 Notifications.defaultProps = {
   displayDrawer: false,
 };
-
 export default Notifications;
