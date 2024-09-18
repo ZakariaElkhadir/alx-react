f6ec9525d478b82098a06536182ba745d2b10b71
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-18 16:11:44 +0100

    Refactor inline styling in Header and Login components

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,12 +1,21 @@
 import React from "react";
 import logo from "../assets/holber_logo.jpg";
-import "./Header.css";
+import { StyleSheet, css } from "aphrodite";
 function Header() {
   return (
-    <header className="App-header">
+    <header className={`App-header ${css(styles.header)}`}>
       <img src={logo} width={150} alt="holberton school" />
       <h1>School dashboard</h1>
     </header>
   );
 }
+const styles = StyleSheet.create({
+  header: {
+    color: '#e0354b',
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    display: 'flex',
+    alignItems: 'center',
+    padding: '16px',
+  }
+})
 export default Header;
--- 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "./Login.css";
+import { StyleSheet, css } from "aphrodite";
 function Login() {
   return (
-    <body className="App-body">
+    <div className={`App-body ${css(styles.Login)}`}>
       <p>Login to access the full dashboard</p>
       <label htmlFor="email"> Email: </label>
       <input type="text" id="email" />
       <label htmlFor="password"> Password: </label>
       <input type="password" id="password" />
       <button>OK</button>
-    </body>
+    </div>
   );
 }
+const styles = StyleSheet.create({
+  Login: {
+    margin: '20px'
+  }
+})
 export default Login;
