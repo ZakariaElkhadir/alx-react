fbe23c2e95c9a0554cdcd1a3e41de43edf80e421
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-18 12:58:58 +0100

    Refactor inline styling in BodySectionWithMarginBottom component

--- 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import BodySection from './BodySection'
+import { StyleSheet, css } from 'aphrodite';
 import './BodySectionWithMarginBottom.css'
 
 import PropTypes from 'prop-types';
 
 const BodySectionWithMarginBottom = (props) => {
-    return (
-     <div className="bodySectionWithMargin">
-          <BodySection {...props} />
-     </div>
-     )
+return (
+ <div className={`bodySectionWithMargin ${css(styles.mBottom)}`}>
+       <BodySection {...props} />
+ </div>
+ )
 }
+const styles = StyleSheet.create({
+     mBottom: {
+          marginBottom: '40px'
+     }
+})
 
 BodySectionWithMarginBottom.propTypes = {
      title: PropTypes.string.isRequired,
