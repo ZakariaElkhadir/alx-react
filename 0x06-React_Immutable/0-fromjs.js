603855887c7d3f25ba1c376530bdc3a164de03bd
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-25 11:07:43 +0100

    Refactor inline styling in setupTests.js and add mock for document object

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,7 @@
+import { fromJS } from "immutable";
+
+function getImmutableObject(params = {}) {
+    const immutableMap = fromJS(params);
+    return immutableMap;
+}
+export default getImmutableObject;
