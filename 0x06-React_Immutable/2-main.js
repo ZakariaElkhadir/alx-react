ae99cd85b75c4d40ad07f24d2c691c32b15942e8
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-25 17:49:27 +0100

    Refactor export statement in 0x06-React_Immutable/2-main.js and add test for accessImmutableObject function

--- 0x06-React_Immutable/2-main.js
+++ 0x06-React_Immutable/2-main.js
@@ -1,15 +1,15 @@
 import accessImmutableObject from './2-nested.js';
 
 const testAccessImmutableObject = () => {
-    const obj = {
-        name: {
-            first: "Guillaume",
-            last: "Salva"
-        }
-    };
-    const path = ['name', 'first'];
-    const result = accessImmutableObject(obj, path);
-    console.log(result); // Should print "Guillaume"
+  const obj = {
+    name: {
+      first: 'Guillaume',
+      last: 'Salva'
+    }
+  };
+  const path = ['name', 'first'];
+  const result = accessImmutableObject(obj, path);
+  console.log(result); // Should print "Guillaume"
 };
 
 testAccessImmutableObject();
