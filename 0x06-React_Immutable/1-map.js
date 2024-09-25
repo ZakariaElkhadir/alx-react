2d901a770fe37542fb18eaff86e4ac4408ecd15e
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-25 12:24:01 +0100

    Refactor export statement in 0-fromjs.js

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,7 +1,7 @@
 #!use/bin/node
 import { Map } from 'immutable';
 function getImmutableObject (params = {}) {
-    const immutableMap = Map(params);
-    return immutableMap;
-  }
-  export default getImmutableObject;
+  const immutableMap = Map(params);
+  return immutableMap;
+}
+export default getImmutableObject;
