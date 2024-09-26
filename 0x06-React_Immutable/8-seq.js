bc416add7a9b4aeab5bae525bac1c42511bde2ae
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-26 19:24:04 +0100

    Refactor printBestStudents function to use immutable Seq for filtering students with scores below 70 and capitalize student names

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,6 +1,18 @@
 import { Seq } from 'immutable';
-function getBestStudents(students) {
-  return Seq(students)
-    .filter(student => student.score < 70);
+
+export default function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  }
+  
+export default function printBestStudents(object) {
+  return Seq(object)
+    .filter(student => student.score >= 70)
+    .map(student => ({
+      ...student,
+      firstName: capitalize(student.firstName),
+      lastName: capitalize(student.lastName)
+    }))
+    .forEach(student => {
+      console.log(`${student.firstName} ${student.lastName}`);
+    });
 }
-export default getBestStudents;
