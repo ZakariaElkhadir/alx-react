f0e07caa8c8936bc355707ab88d564654e5510f1
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-30 18:35:21 +0100

    update

--- 0x05-react_state/task_2/dashboard/src/Header/Header.js
+++ 0x05-react_state/task_2/dashboard/src/Header/Header.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import logo from "../assets/holber_logo.jpg";
 import { StyleSheet, css } from "aphrodite";
+import { AppContext } from "../App/AppContext";
 function Header() {
+  const {user, logOut} = useContext(AppContext)
   return (
-    <header className={`App-header ${css(styles.header)}`}>
-      <img src={logo} width={150} alt="holberton school" />
-      <h1>School dashboard</h1>
-    </header>
+    <div>
+      <header className={`App-header ${css(styles.header)}`}>
+        <img src={logo} width={150} alt="holberton school" />
+        <h1>School dashboard</h1>
+      </header>
+      {user.isLoggedIn && (
+        <section className={css(styles.greeting)} id="logoutSection">
+          Welcome<strong> {user.email} </strong>
+          <em>
+            <a href="#" onClick={logOut}>
+              (logout)
+            </a>
+          </em>
+        </section>
+      )}
+    </div>
   );
 }
 const styles = StyleSheet.create({
@@ -16,6 +30,10 @@ const styles = StyleSheet.create({
     display: 'flex',
     alignItems: 'center',
     padding: '16px',
+  },
+  greeting: {
+    marginTop: "1rem",
   }
-})
+  })
+
 export default Header;
